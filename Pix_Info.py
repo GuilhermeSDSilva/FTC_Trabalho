2f63e8de428ff735477b0761ee0849404c7e042c
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2023-11-29 15:08:31 -0400

    Update Pix_Info.py

--- Pix_Info.py
+++ Pix_Info.py
@@ -1,6 +1,6 @@
 info =input()
 padrao_cnpj = re.compile(r'^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$')
-padrao_numero_ddd = re.compile(r'^\+\d{2}\(\d{2}\)\d{8,9}$')
+padrao_numero_completo = re.compile(r'^\+55\(\d{2}\)\d{4,5}-\d{4}$')
 padrao_cpf = re.compile(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$')
 padrao_chave_rapida = re.compile(r'^\d{2}\.[A-F0-9]{2}\.\d{2}\.[A-F0-9]{2}$')
 padrao_data = re.compile(r'^\d{2}/\d{2}/\d{4}$')
