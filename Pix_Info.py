05e953314389d6c41cea21a469448adc0de3f572
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2023-12-02 12:46:05 -0400

    Update Pix_Info.py

--- Pix_Info.py
+++ Pix_Info.py
@@ -5,3 +5,7 @@ padrao_cpf = re.compile(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$')
 padrao_chave_rapida = re.compile(r'^\d{2}\.[A-F0-9]{2}\.\d{2}\.[A-F0-9]{2}$')
 padrao_data = re.compile(r'^\d{2}/\d{2}/\d{4}$')
 padrao_hora = re.compile(r'^([01][0-9]|2[0-3]):[0-5][0-9]$')
+
+chaves = []
+while True:
+  
