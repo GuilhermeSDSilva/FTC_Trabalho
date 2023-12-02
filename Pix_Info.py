332f0924ee71276f991966c574c6ec5e6a8195d5
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2023-12-02 18:02:15 -0400

    Update Pix_Info.py

--- Pix_Info.py
+++ Pix_Info.py
@@ -1,3 +1,4 @@
+import re
 info =input()
 padrao_cnpj = re.compile(r'^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$')
 padrao_numero_completo = re.compile(r'^\+55\(\d{2}\)\d{4,5}-\d{4}$')
@@ -8,4 +9,21 @@ padrao_hora = re.compile(r'^([01][0-9]|2[0-3]):[0-5][0-9]$')
 padrao_email = re.compile(r'.+@.+')
 chaves = []
 
-  
+inicio=""
+meio="=========="
+final=""
+
+inicio_final=0
+for x in range(len(info)):
+  if info[x]!="=":
+    inicio += info[x]
+  else:
+    inicio_final=x
+    break
+    
+inicio_final+=10
+
+for y in range(inicio_final,len(info)):
+  final+=info[y]
+
+print(inicio)
