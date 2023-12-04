93d8608ca4db200729d70a0fb4b594cf3a3ec255
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2023-12-04 07:11:50 -0400

    Update Pixv2.py

--- Pixv2.py
+++ Pixv2.py
@@ -1,18 +1,20 @@
 import re
 aprovado = True
 info="""04.128.563/0001-10 zxhbpg@jmurip.com +55(92)3656-8985
-62.144.175/0001-20 L2.B3.D5.a7
+62.144.175/0001-20 F2.B3.D5.a7
 ==========
-zxhbpg@jmurip.com 62.144.175/0001-20 R$ 1.000,00 13/12/2022 23:40 s%%B9F7cB19t
-L2.B3.D5.a7 +55(92)3656-8985 R$ 57,52 29/07/2022 13:45 6O31iJa7dZ*%"""
+zxhbpg@jmurip.com 62.144.175/0001-20 R$1.000,00 13/12/2022 23:40 s%%B9F7cB19t
+F2.B3.D5.a7 +55(92)3656-8985 R$57,52 29/07/2022 13:45 6O31iJa7dZ*%"""
 
 padrao_cnpj = re.compile(r'^\d{2,3}\.\d{3}\.\d{3}/\d{4}-\d{2}$')
 padrao_telefone= re.compile(r'^\+55\(\d{2}\)\d{4,5}-\d{4}$')
 padrao_cpf = re.compile(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$')
-padrao_chaves_rapidas = re.compile(r'^([0-9A-Fa-f]{2}\.){3}[0-9A-Fa-f]{2}$')
-padrao_data = re.compile(r'^\d{2}/\d{2}/\d{4}$')
+padrao_chaves_rapidas = re.compile(r"^(?!.*[A-Za-z]{2})(?!.*([0-9A-Fa-f])\1)[0-9A-Fa-f]([0-9A-Fa-f](?![A-Za-z]))?\.[0-9A-Fa-f]{2}(\.[0-9A-Fa-f]{2}){2}$")
+padrao_data = re.compile(r"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$")
 padrao_hora = re.compile(r'^([01][0-9]|2[0-3]):[0-5][0-9]$')
 padrao_email = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+padrao_codigo = re.compile(r"^(?=(?:.*[A-Z]){3})(?=(?:.*[a-z]){3})(?=(?:.*[$%@(,[*]){2})(?=(?:.*\d){4}).{12}$")
+padrao_dinheiro = re.compile(r"R\$\s\d{1,3}(?:\.\d{3})*(?:,\d{2})")
 chaves = []
 
 inicio=""
@@ -30,7 +32,6 @@ final = info[inicio_final:]
 # Verificar se 'inicio' não está vazio antes de dividir
 if inicio:
     inicio = inicio.split()
-   
     for z in range(len(inicio)):
         cnpj_encontrado = padrao_cnpj.findall(inicio[z])
         chaves += cnpj_encontrado
@@ -40,22 +41,30 @@ if inicio:
         chaves += email_encontrado
         telefone_encontrado = padrao_telefone.findall(inicio[z])
         chaves += telefone_encontrado
-        chave_encontrado = padrao_chaves_rapidas.findall(inicio[z])
-        chaves += chave_encontrado
+        chave_encontrado = padrao_chaves_rapidas.match(inicio[z])
+        if chave_encontrado:
+            chaves.append(chave_encontrado.group())
     
 
     if final:
         linhas_final = final.split('\n')
-# Verificar se todas as chaves de cada linha estão na lista chaves
-        for i in range(1, len(linhas_final)):  # Começar a partir da segunda linha (índice 1)
+
+        for i in range(1, len(linhas_final)): 
             partes_linha = linhas_final[i].split()
-            if len(partes_linha) >= 2 and partes_linha[0] in chaves and partes_linha[1] in chaves:
-                continue  # As duas primeiras strings estão na lista chaves
+            
+            dinheiro_valida = padrao_dinheiro.match(partes_linha[2])
+            data_valida = padrao_data.match(partes_linha[3])
+            hora_valida = padrao_hora.match(partes_linha[4])
+            codigo_valida = padrao_codigo.match(partes_linha[5])
+            
+            
+            
+            if ((len(partes_linha) >= 2) and (partes_linha[0] in chaves) and (partes_linha[1] in chaves) and (data_valida) and(data_valida) and(codigo_valida)):
+                continue  
             else:
                 aprovado = False
                 break
            
-    
     else:
         aprovado=False           
 else:
@@ -66,4 +75,3 @@ if aprovado==True:
 else:
     print("False",end="")
     
-print(chaves)
