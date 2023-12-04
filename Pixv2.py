e8046a0c4819dc5c582071352be4beea4fd4f798
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2023-12-04 08:07:49 -0400

    Update Pixv2.py

--- Pixv2.py
+++ Pixv2.py
@@ -1,10 +1,14 @@
 import re
 aprovado = True
-info="""04.128.563/0001-10 zxhbpg@jmurip.com +55(92)3656-8985
-62.144.175/0001-20 F2.B3.D5.a7
+info="""04.280.196/0001-76 uea@uea.edu.br +55(92)3348-7601
+
+09.628.825/0001-20 03.A4.2B.F8
+
 ==========
-zxhbpg@jmurip.com 62.144.175/0001-20 R$1.000,00 13/12/2022 23:40 s%%B9F7cB19t
-F2.B3.D5.a7 +55(92)3656-8985 R$57,52 29/07/2022 13:45 6O31iJa7dZ*%"""
+
+uea@uea.edu.br 09.628.825/0001-20 R$ 1.000.000,00 09/01/2023 09:03 ABC@@1234abc
+
+03.A4.2B.F8 +55(92)3348-7601 R$ 2.000.000,00 08/01/2022 10:10 FG@H*12ab34c"""
 
 padrao_cnpj = re.compile(r'^\d{2,3}\.\d{3}\.\d{3}/\d{4}-\d{2}$')
 padrao_telefone= re.compile(r'^\+55\(\d{2}\)\d{4,5}-\d{4}$')
@@ -14,7 +18,7 @@ padrao_data = re.compile(r"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$")
 padrao_hora = re.compile(r'^([01][0-9]|2[0-3]):[0-5][0-9]$')
 padrao_email = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 padrao_codigo = re.compile(r"^(?=(?:.*[A-Z]){3})(?=(?:.*[a-z]){3})(?=(?:.*[$%@(,[*]){2})(?=(?:.*\d){4}).{12}$")
-padrao_dinheiro = re.compile(r"R\$\s\d{1,3}(?:\.\d{3})*(?:,\d{2})")
+padrao_dinheiro = re.compile(r'^R\$\s\d{1,3}(?:\.\d{3})*(?:,\d{2})?$')
 chaves = []
 
 inicio=""
@@ -52,14 +56,14 @@ if inicio:
         for i in range(1, len(linhas_final)): 
             partes_linha = linhas_final[i].split()
             
-            dinheiro_valida = padrao_dinheiro.match(partes_linha[2])
-            data_valida = padrao_data.match(partes_linha[3])
-            hora_valida = padrao_hora.match(partes_linha[4])
-            codigo_valida = padrao_codigo.match(partes_linha[5])
             
+            dinheiro_valida = padrao_dinheiro.match(partes_linha[2]+" "+partes_linha[3])
+            data_valida = padrao_data.match(partes_linha[4])
+            hora_valida = padrao_hora.match(partes_linha[5])
+            codigo_valida = padrao_codigo.match(partes_linha[6])
             
-            
-            if ((len(partes_linha) >= 2) and (partes_linha[0] in chaves) and (partes_linha[1] in chaves) and (data_valida) and(data_valida) and(codigo_valida)):
+         
+            if ((len(partes_linha) >= 2) and (partes_linha[0] in chaves) and (partes_linha[1] in chaves) and (dinheiro_valida) and (data_valida) and(hora_valida) and(codigo_valida)):
                 continue  
             else:
                 aprovado = False
